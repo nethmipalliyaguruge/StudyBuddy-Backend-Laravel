cbea58333ce015fddc2e0316e66afec0390fb296
nethmipalliyaguruge <cb015790@students.apiit.lk>
2026-02-01 21:04:06 +0530

    Add favicon link to all layout files

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,6 +7,9 @@
 
     <title>{{ $title ?? config('app.name', 'StudyBuddy') }}</title>
 
+    <!-- Favicon -->
+    <link rel="icon" type="image/x-icon" href="{{ asset('favicon.ico') }}">
+
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
